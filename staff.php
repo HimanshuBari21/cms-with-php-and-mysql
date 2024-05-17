a8e40a516b511b419c373815ae03204ed28d3c71
HimanshuBari21 <himanshubari789@gmail.com>
2024-05-17 10:33:26 +0530

    feature/Added Staff Page WIP

--- staff.php
+++ staff.php
@@ -19,10 +19,18 @@ include 'include/header.php'
 
     <!-- navigation -->
     <nav class="md:w-1/4">
-        <?php
-        navigation($subjectId, $pageId, false)
-        ?>
+
     </nav>
+
+    <section class="md:w-3/4">
+        <legend class="font-bold text-3xl">Welcome to Staff Area</legend>
+        <br>
+        <ul class="ml-2">
+            <li> - <a class="hover:underline" href="/cms-with-php-and-mysql/content.php">Manage Content</a></li>
+            <li> - <a class="hover:underline" href="/cms-with-php-and-mysql/content.php">Add Staff User</a></li>
+            <li> - <a class="hover:underline" href="/cms-with-php-and-mysql/content.php">Logout</a></li>
+        </ul>
+    </section>
 
 </main>
 
